45ae3854cc7935d2b1c74bbf5bcc63e980ab1a3f
Jake Houser <jakob.houser@gmail.com>
2018-04-14 11:35:16 +0200

    Load fixture data into forecast

--- src/containers/forecast.js
+++ src/containers/forecast.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
 import {Row, Col} from 'reactstrap';
 import WeatherCard from "../components/weatherCard";
+import forecastData from '../fixtures/forecast.json'
 
 class Forecast extends Component {
+    componentDidMount() {
+        this.setState({data: forecastData})
+    }
     render() {
         return <Row>
             <Col sm={12} md={2}>
