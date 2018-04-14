d5aaeace628c0fe85670cb5cb03093273379b156
Jake Houser <jakob.houser@gmail.com>
2018-04-14 11:21:25 +0200

    Move row to SearchBar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -7,15 +7,17 @@ const searchStyles = {
 
 class SearchBar extends Component {
     render() {
-        return <Col style={searchStyles} sm={12} md={6}>
-            <Form>
-                <FormGroup>
-                    <Label for="location">Location</Label>
-                    <Input type="text" name="location" id="location" placeholder="Enter your location!"/>
-                </FormGroup>
-            </Form>
-            <Button>Submit</Button>
-        </Col>
+        return <Row>
+            <Col style={searchStyles} sm={12} md={6}>
+                <Form>
+                    <FormGroup>
+                        <Label for="location">Location</Label>
+                        <Input type="text" name="location" id="location" placeholder="Enter your location!"/>
+                    </FormGroup>
+                </Form>
+                <Button>Submit</Button>
+            </Col>
+        </Row>
     }
 }
 
