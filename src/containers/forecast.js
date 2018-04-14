d941a3b9d680954d12deb1f8e517adc840f6a587
Jake Houser <jakob.houser@gmail.com>
2018-04-14 11:37:22 +0200

    Delete weather card

--- src/containers/forecast.js
+++ src/containers/forecast.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Row, Col} from 'reactstrap';
-import WeatherCard from "../components/weatherCard";
 import forecastData from '../fixtures/forecast.json'
 
 class Forecast extends Component {
@@ -10,19 +9,7 @@ class Forecast extends Component {
     render() {
         return <Row>
             <Col sm={12} md={2}>
-                <WeatherCard/>
-            </Col>
-            <Col sm={12} md={2}>
-                <WeatherCard/>
-            </Col>
-            <Col sm={12} md={2}>
-                <WeatherCard/>
-            </Col>
-            <Col sm={12} md={2}>
-                <WeatherCard/>
-            </Col>
-            <Col sm={12} md={2}>
-                <WeatherCard/>
+
             </Col>
         </Row>
     }
