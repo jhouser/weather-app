ac2b1db6e1fe73ebbdadde4dc33645300fcaaa5a
Jake Houser <jakob.houser@gmail.com>
2018-04-15 11:23:07 +0200

    Add loading indicator if data isn't present

--- src/containers/current.js
+++ src/containers/current.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Current from "../components/current";
 import {currentWeather} from "../reducers/weather";
-
+import {Loading} from '../components/loading';
 
 class CurrentContainer extends Component {
     render() {
         const weather = this.props.data || {};
         if (!this.props.data) {
-            return <div></div>
+            return <Loading />
         }
         return <Current data={weather}/>
 
