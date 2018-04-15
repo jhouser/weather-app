7fcbf3162b5cbe6abb1b91672bfe6774bb479f56
Jake Houser <jakob.houser@gmail.com>
2018-04-15 10:41:03 +0200

    Add action for getting current weather

---
+++ src/actions/weather.js
@@ -0,0 +1,19 @@
+import { RSAA } from 'redux-api-middleware';
+
+export const CURRENT_REQUEST = '@@weather/CURRENT_REQUEST';
+export const CURRENT_SUCCESS = '@@weather/CURRENT_SUCCESS';
+export const CURRENT_FAILURE = '@@weather/CURRENT_FAILURE';
+
+const weather_api_endpoint = 'api.openweathermap.org/data/2.5/weather';
+const api_key = 'f7172877468a211c11a3b5c33f5338c8';
+
+export const current = (location) => ({
+  [RSAA]: {
+    endpoint: weather_api_endpoint + '/weather?q=' + location + '&units=metric&appid=' + api_key,
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    types: [
+      CURRENT_REQUEST, CURRENT_SUCCESS, CURRENT_FAILURE
+    ]
+  }
+});
