a7781313e7a3feb2644e6a15e619f117caf61cc4
Jake Houser <jakob.houser@gmail.com>
2018-04-15 11:19:43 +0200

    Fix spacing in action

--- src/actions/weather.js
+++ src/actions/weather.js
@@ -1,4 +1,4 @@
-import { RSAA } from 'redux-api-middleware';
+import {RSAA} from 'redux-api-middleware';
 
 export const CURRENT_REQUEST = '@@weather/CURRENT_REQUEST';
 export const CURRENT_SUCCESS = '@@weather/CURRENT_SUCCESS';
@@ -8,11 +8,11 @@ const weather_api_endpoint = 'http://api.openweathermap.org/data/2.5/';
 const api_key = 'f7172877468a211c11a3b5c33f5338c8';
 
 export const current = (location) => ({
-  [RSAA]: {
-    endpoint: weather_api_endpoint + 'weather?q=' + location + '&units=metric&appid=' + api_key,
-    method: 'GET',
-    types: [
-      CURRENT_REQUEST, CURRENT_SUCCESS, CURRENT_FAILURE
-    ]
-  }
+    [RSAA]: {
+        endpoint: weather_api_endpoint + 'weather?q=' + location + '&units=metric&appid=' + api_key,
+        method: 'GET',
+        types: [
+            CURRENT_REQUEST, CURRENT_SUCCESS, CURRENT_FAILURE
+        ]
+    }
 });
